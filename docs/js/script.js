6efdeef9388d1f15a90557243f8788ac9e797cc6
mahdihassani9374 <mahdihassani9374@gmail.com>
2019-07-29 10:54:27 +0430

    add persian calender

--- docs/js/script.js
+++ docs/js/script.js
@@ -5,4 +5,38 @@ $(function () {
   $('.grid').isotope({
     itemSelector: '.grid-item',
   });
+
+  var customOptions = {
+    placeholder: "روز / ماه / سال"
+    , twodigit: true
+    , closeAfterSelect: true
+    , nextButtonIcon: "fa fa-arrow-circle-right"
+    , previousButtonIcon: "fa fa-arrow-circle-left"
+    , buttonsColor: "blue"
+    , forceFarsiDigits: true
+    , markToday: true
+    , markHolidays: true
+    , highlightSelectedDay: true
+    , sync: true
+    , gotoToday: true
+    , pastYearsCount: 0
+    ,  futureYearsCount: 3,
+        startView: 'month',
+        minView: 'month',
+        startDate:"today"
+  }
+  
+  kamaDatepicker('date-1',customOptions);
+  kamaDatepicker('date-2',customOptions);
+  kamaDatepicker('date-3',customOptions);
+  kamaDatepicker('date-4',customOptions);
+
+  $('#bd-next-date-1').css('color','#3bac10')
+  $('#bd-next-date-2').css('color','#3bac10')
+  $('#bd-next-date-3').css('color','#3bac10')
+  $('#bd-next-date-4').css('color','#3bac10')
+  $('#bd-prev-date-1').css('color','#3bac10')
+  $('#bd-prev-date-2').css('color','#3bac10')
+  $('#bd-prev-date-3').css('color','#3bac10')
+  $('#bd-prev-date-4').css('color','#3bac10')
 })
