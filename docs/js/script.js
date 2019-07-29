547a64626d9a4e7a4ca90b5e495661411c77b223
mahdihassani9374 <mahdihassani9374@gmail.com>
2019-07-29 11:23:04 +0430

    vue js airlien

--- docs/js/script.js
+++ docs/js/script.js
@@ -25,7 +25,7 @@ $(function () {
         minView: 'month',
         startDate:"today"
   }
-  
+
   kamaDatepicker('date-1',customOptions);
   kamaDatepicker('date-2',customOptions);
   kamaDatepicker('date-3',customOptions);
@@ -35,8 +35,28 @@ $(function () {
   $('#bd-next-date-2').css('color','#3bac10')
   $('#bd-next-date-3').css('color','#3bac10')
   $('#bd-next-date-4').css('color','#3bac10')
+  $('#bd-next-date-5').css('color','#3bac10')
+  $('#bd-next-date-6').css('color','#3bac10')
   $('#bd-prev-date-1').css('color','#3bac10')
   $('#bd-prev-date-2').css('color','#3bac10')
   $('#bd-prev-date-3').css('color','#3bac10')
   $('#bd-prev-date-4').css('color','#3bac10')
+  $('#bd-prev-date-5').css('color','#3bac10')
+  $('#bd-prev-date-6').css('color','#3bac10')
+})
+
+var app = new Vue({
+  el:'#app',
+  data:{
+    air: {
+      name:'Inside',
+      type:0
+    }
+  },
+  methods: {
+    changeAir:function(name) {     
+      this.air.name=name;
+      this.air.type=0;
+    }
+  },
 })
